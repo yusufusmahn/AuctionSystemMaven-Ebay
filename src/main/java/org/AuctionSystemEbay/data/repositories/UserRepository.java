2a0ed4c309b9f4fee4927a9471ba6546318fda06
yusufusman <yusuf.usman1003@gmail.com>
2025-06-07 11:15:24 +0100

    task update

--- src/main/java/org/AuctionSystemEbay/data/repositories/UserRepository.java
+++ src/main/java/org/AuctionSystemEbay/data/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package org.AuctionSystemEbay.data.repositories;
 import org.AuctionSystemEbay.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface Users extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
 }
