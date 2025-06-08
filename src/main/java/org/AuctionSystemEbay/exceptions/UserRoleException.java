462ac3c23633ef5df9d058b9c00512f4136ce964
yusufusman <yusuf.usman1003@gmail.com>
2025-06-08 19:29:15 +0100

    task update

---
+++ src/main/java/org/AuctionSystemEbay/exceptions/UserRoleException.java
@@ -0,0 +1,7 @@
+package org.AuctionSystemEbay.exceptions;
+
+public class UserRoleException extends AuctionSystemException {
+    public UserRoleException(String message) {
+        super(message);
+    }
+}
