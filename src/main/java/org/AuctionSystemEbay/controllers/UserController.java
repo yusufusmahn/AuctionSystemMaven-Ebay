fc430812b1b4b0b5057a7da26c079c1b7fca1165
yusufusman <yusuf.usman1003@gmail.com>
2025-06-09 20:06:39 +0100

    task update

---
+++ src/main/java/org/AuctionSystemEbay/controllers/UserController.java
@@ -0,0 +1,51 @@
+package org.AuctionSystemEbay.controllers;
+
+import org.AuctionSystemEbay.dtos.requests.*;
+import org.AuctionSystemEbay.dtos.responses.*;
+import org.AuctionSystemEbay.exceptions.AuctionSystemException;
+import org.AuctionSystemEbay.services.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest) {
+        try {
+            UserResponse response = userService.createUser(userRequest);
+            return new ResponseEntity<>(new ApiResponse(response, true), HttpStatus.CREATED);
+        } catch (AuctionSystemException e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{userId}/role")
+    public ResponseEntity<?> updateUserRole(@PathVariable("userId") String userId, @RequestBody UpdateRoleRequest request) {
+        try {
+            UserResponse response = userService.updateUserRole(userId, request);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+
+        } catch (AuctionSystemException e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(),false), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            LoginResponse response = userService.login(loginRequest);
+            return new ResponseEntity<>(new ApiResponse(response, true), HttpStatus.OK);
+        } catch (AuctionSystemException e) {
+            return new ResponseEntity<>(new ApiResponse(e.getMessage(), false), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
