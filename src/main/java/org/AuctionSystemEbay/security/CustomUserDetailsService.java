ffeeeb22f117d639a6c64e3ae49fbb679ada881b
yusufusman <yusuf.usman1003@gmail.com>
2025-06-13 10:33:44 +0100

    task update

--- src/main/java/org/AuctionSystemEbay/security/CustomUserDetailsService.java
+++ src/main/java/org/AuctionSystemEbay/security/CustomUserDetailsService.java
@@ -1,8 +1,5 @@
 package org.AuctionSystemEbay.security;
 
-import org.AuctionSystemEbay.data.models.User;
-import org.AuctionSystemEbay.data.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,19 +8,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email.toLowerCase());
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found with email: " + email);
-        }
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getEmail())
-                .password(user.getPassword())
-                .roles(user.getRole())
-                .build();
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
